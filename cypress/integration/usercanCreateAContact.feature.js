82a4f678de50fe20dfc1a44898fa6f04dd72300e
shagufta <shagufta.batool8@gmail.com>
2021-11-12 15:44:41 +0100

    testing index.html via cypress

--- cypress/integration/usercanCreateAContact.feature.js
+++ cypress/integration/usercanCreateAContact.feature.js
@@ -3,5 +3,11 @@
 describe('user can create a contact', () => {
     it('test', () => {
         cy.visit('http://localhost:3474')
+        cy.get('#add-contact').click()
+        cy.get('#name').type('Shagufta')
+		cy.get('#email').type('shagufta@craft.se')
+		cy.get('#phone').type('0700 101010')
+		cy.get('#job').type('self employed')
+		cy.get('#notes').type('Beginner coder')
     })
 })
